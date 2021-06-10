5ec7319e49b480436c371a9c58686af927a3fab5
Junichi Kato <j5ik2o@gmail.com>
2021-06-10 16:32:26 +0900

    wip

--- src/main/scala/com/github/j5ik2o/authnauthz/oidc/AbstractTokenFailureResponse.scala
+++
@@ -1,7 +0,0 @@
-package com.github.j5ik2o.authnauthz.oidc
-
-abstract class AbstractTokenFailureResponse(
-    val error: ErrorType,
-    val errorDescription: Option[String],
-    val errorUri: Option[String]
-) extends TokenResponse
--- src/main/scala/com/github/j5ik2o/authnauthz/oidc/implicit/AuthenticationImplicitRequest.scala
+++ src/main/scala/com/github/j5ik2o/authnauthz/oidc/implicit/AuthenticationImplicitRequest.scala
@@ -3,7 +3,7 @@ package com.github.j5ik2o.authnauthz.oidc.`implicit`
 import com.github.j5ik2o.authnauthz.oauth2.{ AbstractAuthorizationRequest, ClientId, RedirectURI, State }
 import com.github.j5ik2o.authnauthz.oidc.{ Nonce, ResponseTypes, Scopes }
 
-final case class AuthorizationImplicitRequest(
+final case class AuthenticationImplicitRequest(
     override val responseTypes: ResponseTypes,
     override val clientId: ClientId,
     override val redirectURI: RedirectURI,
