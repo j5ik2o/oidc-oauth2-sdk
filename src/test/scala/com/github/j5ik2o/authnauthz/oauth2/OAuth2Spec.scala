0934a5d41f859beb5c17c82ec47cbd8af886ba15
Junichi Kato <j5ik2o@gmail.com>
2021-07-14 21:15:53 +0900

    wip

--- src/test/scala/com/github/j5ik2o/authnauthz/oauth2/OAuth2Spec.scala
+++ src/test/scala/com/github/j5ik2o/authnauthz/oauth2/OAuth2Spec.scala
@@ -1,12 +1,5 @@
 package com.github.j5ik2o.authnauthz.oauth2
 
-import com.github.j5ik2o.authnauthz.RequestHandler.RequestHandlerOps
-import com.github.j5ik2o.authnauthz.oauth2.code.{
-  AuthorizationCodeRequest,
-  AuthorizationCodeRequestPlain,
-  AuthorizationCodeRequestValid,
-  AuthorizationCodeResponse
-}
 import org.scalatest.freespec.AnyFreeSpec
 
 /** https://openid-foundation-japan.github.io/rfc6749.ja.html
